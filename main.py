f41a00382cf269bcf6488829cd061c4e61464a13
Laxman Basumatary <shadygrey23@gmail.com>
2026-09-10 11:35:05 +0530

    Fix animation callback

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import math
+
 from pyscript import window
+from pyodide.ffi import create_proxy
 
 
+# Get canvas
 canvas = window.document.getElementById("canvas")
 ctx = canvas.getContext("2d")
 
-
+# Animation variable
 t = 0
 
 
 def animate(timestamp):
-
     global t
 
-    # Clear the canvas
+    # Clear canvas
     ctx.clearRect(0, 0, 800, 400)
 
     # Calculate position
@@ -33,11 +35,15 @@ def animate(timestamp):
     ctx.fillStyle = "red"
     ctx.fill()
 
-    # Move animation forward
+    # Move animation
     t += 0.03
 
-    # Request next frame
-    window.requestAnimationFrame(animate)
+    # Schedule next frame
+    window.requestAnimationFrame(animation_proxy)
 
 
-window.requestAnimationFrame(animate)
+# Create a persistent JavaScript callback
+animation_proxy = create_proxy(animate)
+
+# Start animation
+window.requestAnimationFrame(animation_proxy)
